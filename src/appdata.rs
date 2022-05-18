3fcc11e0e5670720b69ee057e808d33baa7d66ef
earlgray <earlgray283@gmail.com>
2022-05-19 02:36:48 +0900

    feat: setup

--- src/appdata.rs
+++ src/appdata.rs
@@ -1,9 +1,64 @@
+use crate::api::Client;
 use anyhow::{anyhow, Result};
+use spinners::{Spinner, Spinners};
 use std::{
+    env, error,
     fs::File,
-    io::{BufRead, BufReader, Write},
+    io::{stdin, stdout, BufRead, BufReader, Write},
     path::PathBuf,
+    str::FromStr,
 };
+
+pub async fn setup() -> Result<(String, String)> {
+    eprintln!("You seem to login for the first time. Please input your account information.");
+    Ok(loop {
+        let username: String = loop {
+            let u = input_with_prompt::<String>("username: ").unwrap();
+            if !u.is_empty() {
+                break u;
+            }
+        };
+        let password: String = loop {
+            print!("password: ");
+            stdout().flush().unwrap();
+            let p = rpassword::read_password().unwrap();
+            if !p.is_empty() {
+                break p;
+            }
+        };
+
+        dbg!(&username, &password);
+
+        let mut sp = Spinner::new(Spinners::Dots9, "Waiting for login...".to_string());
+        match Client::with_login(&username, &password).await {
+            Ok(_) => {
+                sp.stop_with_newline();
+                save_account_info(&username, &password)?;
+                break (username, password);
+            }
+            Err(_) => {
+                sp.stop_with_newline();
+                eprintln!("Failed to login. Maybe Username or Password is invalid");
+            }
+        }
+    })
+}
+
+pub fn save_account_info(username: &str, password: &str) -> Result<()> {
+    let mut config_dir = config_dir().ok_or_else(|| anyhow!("could not find config dir"))?;
+    config_dir.push(".env");
+    let mut f = File::create(config_dir)?;
+    writeln!(f, "CVPN_USERNAME={}", username)?;
+    writeln!(f, "CVPN_PASSWORD={}", password)?;
+    Ok(())
+}
+
+pub fn load_account_info() -> Result<(String, String)> {
+    let mut config_dir = config_dir().ok_or_else(|| anyhow!("could not find config dir"))?;
+    config_dir.push(".env");
+    dotenv::from_filename(config_dir)?;
+    Ok((env::var("CVPN_USERNAME")?, env::var("CVPN_PASSWORD")?))
+}
 
 pub fn save_cookies(cookies: &Vec<String>) -> Result<()> {
     let mut cache_dir = cache_dir().ok_or_else(|| anyhow!("could not find cache dir"))?;
@@ -45,7 +100,7 @@ pub fn user_config_dir() -> Option<PathBuf> {}
 
 #[cfg(target_os = "macos")]
 pub fn cache_dir() -> Option<PathBuf> {
-    use std::{env, fs::create_dir_all, str::FromStr};
+    use std::fs::create_dir_all;
     let home = match env::var_os("HOME") {
         Some(home) => home,
         None => return None,
@@ -54,4 +109,28 @@ pub fn cache_dir() -> Option<PathBuf> {
     p.push("Library/Caches/cvpn-rs");
     create_dir_all(&p).unwrap();
     Some(p)
+}
+
+#[cfg(target_os = "macos")]
+pub fn config_dir() -> Option<PathBuf> {
+    use std::fs::create_dir_all;
+    let home = match env::var_os("HOME") {
+        Some(home) => home,
+        None => return None,
+    };
+    let mut p = PathBuf::from_str(home.to_str().unwrap()).unwrap();
+    p.push("Library/Application Support/cvpn-rs");
+    create_dir_all(&p).unwrap();
+    Some(p)
+}
+
+fn input_with_prompt<T: FromStr>(prompt: &str) -> Result<T, Box<dyn error::Error>>
+where
+    <T as FromStr>::Err: std::error::Error,
+{
+    print!("{}", prompt);
+    stdout().flush()?;
+    let mut buf = String::new();
+    stdin().read_line(&mut buf)?;
+    Ok(buf.trim().parse().map_err(|_e| "")?)
 }
